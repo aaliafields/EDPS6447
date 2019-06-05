ba86a26ef41abd31e078a4a96b9b83a0426302d1
aaliafields <46548050+aaliafields@users.noreply.github.com>
2019-06-04 20:09:02 -0600

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,39 @@
+var element1=document.getElementById("view_home");
+    console.log(element1);
+
+var element2=document.getElementById("view_class_newsfeed");
+    console.log(element2);
+
+var element3=document.getElementById("view_student_spotlight");
+    console.log(element3);
+
+var element4=document.getElementById("view_activity_gallery");
+    console.log(element4);
+
+var element11=document.getElementById("view_contact_me");
+
+var element5=document.getElementById("view_gallery1");
+    console.log(element5);
+
+var element6=document.getElementById("view_gallery2");
+    console.log(element6);
+
+var element7=document.getElementById("view_gallery3");
+    console.log(element7);
+
+var element8=document.getElementById("view_class_newsfeed1");
+    console.log(element8);
+
+var element9=document.getElementById("view_class_newsfeed2");
+    console.log(element9);
+
+var element10=document.getElementById("view_class_newsfeed3");
+    console.log(element10);
+
+
+
+
+
+    
+
+
