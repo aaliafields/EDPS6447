369e7d670a02c9ec6583ebaf51671d437d8cdb0a
aaliafields <46548050+aaliafields@users.noreply.github.com>
2019-07-03 13:54:05 -0600

    Add files via upload

---
+++ EDPS6447-master/js/variable.js
@@ -0,0 +1,40 @@
+var view_home=document.getElementById("view_home");
+    console.log(view_home);
+
+var view_class_newsfeed=document.getElementById("view_class_newsfeed");
+    console.log(view_class_newsfeed);
+
+var view_student_spotlight=document.getElementById("view_student_spotlight");
+    console.log(view_student_spotlight);
+
+var view_activity_gallery=document.getElementById("view_activity_gallery");
+    console.log(view_activity_gallery);
+
+var view_contact_me=document.getElementById("view_contact_me");
+    console.log(view_contact_me);
+
+var view_gallery1=document.getElementById("view_gallery1");
+    console.log(view_gallery1);
+
+var view_gallery2=document.getElementById("view_gallery2");
+    console.log(view_gallery2);
+
+var view_gallery3=document.getElementById("view_gallery3");
+    console.log(view_gallery3);
+
+var view_class_newsfeed1=document.getElementById("view_class_newsfeed1");
+    console.log(view_class_newsfeed1);
+
+var view_class_newsfeed2=document.getElementById("view_class_newsfeed2");
+    console.log(view_class_newsfeed2);
+
+var view_classnewsfeed3=document.getElementById("view_class_newsfeed3");
+    console.log(view_classnewsfeed3);
+
+
+
+
+
+    
+
+
